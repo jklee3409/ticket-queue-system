883c514d6c533c822f09ad4d6d254679fc2fe30a
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-09-19 22:03:16 +0900

    feat: Queue Worker 스케쥴러 구현

--- src/main/java/com/mycom/myapp/TicketQueueApplication.java
+++ src/main/java/com/mycom/myapp/TicketQueueApplication.java
@@ -2,7 +2,9 @@ package com.mycom.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class TicketQueueApplication {
 
---
+++ src/main/java/com/mycom/myapp/scheduler/QueueScheduler.java
@@ -0,0 +1,65 @@
+package com.mycom.myapp.scheduler;
+
+import com.mycom.myapp.constant.RedisConstant;
+import com.mycom.myapp.service.ActiveUserService;
+import com.mycom.myapp.service.SseService;
+import com.mycom.myapp.service.WaitingQueueService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class QueueScheduler {
+
+    @Value("${queue.settings.max-active-users}")
+    private Long MAX_ACTIVE_USERS;
+
+    private final WaitingQueueService waitingQueueService;
+    private final ActiveUserService activeUserService;
+    private final SseService sseService;
+
+    @Scheduled(fixedDelay = 3000)
+    public void processQueue() {
+        activeUserService.popRandomActiveUsers();
+
+        Long currentActiveCount = activeUserService.getActiveUserCount();
+        if (currentActiveCount == null) {
+            log.error("Active user set (key: {}) does not exist in Redis.", RedisConstant.ACTIVE_USERS_KEY);
+            return;
+        }
+        long possibleActiveCount = MAX_ACTIVE_USERS - currentActiveCount;
+        if (possibleActiveCount <= 0) return;
+
+        List<String> movedUsers = waitingQueueService.moveTopNUsersToActiveQueue(possibleActiveCount);
+
+        if (movedUsers != null && !movedUsers.isEmpty()) {
+            movedUsers.forEach(userId -> {
+                sseService.sentToClient(
+                        userId,
+                        "ACTIVE_TURN",
+                        "The wait is over."
+                );
+            });
+
+            List<String> waitingUsers = waitingQueueService.getWaitingUsersInOrder();
+
+            if (waitingUsers == null || waitingUsers.isEmpty()) return;
+
+            for (int i = 0; i < waitingUsers.size(); i++) {
+                String userId = waitingUsers.get(i);
+                long currentRank = i + 1;
+
+                sseService.sentToClient(
+                        userId,
+                        "RANK_UPDATE",
+                        currentRank
+                );
+            }
+        }
+    }
+}
